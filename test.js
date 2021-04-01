38e098020ab0b6a691739c17a5319067022fddd9
Drew Wyatt <drew.j.wyatt@gmail.com>
2021-04-01 15:51:49 -0400

    use inputs as json

--- test.js
+++ test.js
@@ -1,3 +1,8 @@
 #! /usr/bin/env node
 
-console.log('env.SOME_KEY:', process.env.SOME_KEY)
+const INPUTS_AS_JSON = process.env.INPUTS_AS_JSON;
+const { project, config } = JSON.parse(INPUTS_AS_JSON);
+
+console.log('project', project);
+console.log('config', config);
+console.log('env.SOME_KEY:', process.env.SOME_KEY);
